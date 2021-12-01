1d35565d0e5042122997ba53f9516716796516e4
Salvador Chávez <Salvador Chvez@Chavador>
2021-11-30 22:51:40 -0600

    Se borró todo alv

--- src/eda2_proyecto2_eq6_g5/Main.java
+++ src/eda2_proyecto2_eq6_g5/Main.java
@@ -10,23 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Esto es una prueba.");
-        System.out.println("Esta es la prueba hecha desde VisualStudio");
-        System.out.println("Jimeno miamor");
-        System.out.println("Tae miamor");
-        System.out.println("PRUEBA :)");
-        System.out.println("PRUEBA 2 :)");
-         System.out.println("Prueba desde rama CUCO");
-        System.out.println("CUCO ENSEÑANDO EL SIX PACK");
-        System.out.println("JIMENO TEAMO"); //editar codigo
-
-
-
-        //Esta es una prueba ojO
-
-        //Cambio
-
-        //Tista ya sueltame por favor :(
+        
     }
     
 }
