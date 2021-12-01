1f8ea282b9777544ab965777851b1347852ed247
Vanessa Nava <95328400+VaneNv@users.noreply.github.com>
2021-11-30 21:18:08 -0600

    Esto es una prueba

--- src/eda2_proyecto2_eq6_g5/Main.java
+++ src/eda2_proyecto2_eq6_g5/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba.");
         System.out.println("Esta es la prueba hecha desde VisualStudio");
+        System.out.println("Jimeno miamor");
     }
     
 }
