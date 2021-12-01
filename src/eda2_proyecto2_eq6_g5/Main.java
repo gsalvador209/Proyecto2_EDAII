299dd8cb20cb09c211fe7613950f79769e05ec93
gsalvador209 <dchavezvillanueva@gmail.com>
2021-11-30 20:54:30 -0600

    Se hizo una prueba desde VS

--- src/eda2_proyecto2_eq6_g5/Main.java
+++ src/eda2_proyecto2_eq6_g5/Main.java
@@ -11,6 +11,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Esto es una prueba.");
+        System.out.println("Esta es la prueba hecha desde VisualStudio");
     }
     
 }
