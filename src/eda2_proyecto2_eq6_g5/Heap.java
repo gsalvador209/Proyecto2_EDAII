07697e0655389125b8548997ac33964138c7a87a
VaneNv <vn.nv.al37@gmail.com>
2021-11-30 23:12:41 -0600

    Ultima prueba

--- src/eda2_proyecto2_eq6_g5/Heap.java
+++ src/eda2_proyecto2_eq6_g5/Heap.java
@@ -1,7 +1,21 @@
 package eda2_proyecto2_eq6_g5;
 
+// Nava Alberto Vanessa 
+
 public class Heap {
+
     public Heap(){
+
         System.out.println("Heap :)");
+        System.out.println("Aquí va el heap");
+    
+        /*
+        *   Aqui va todo el código del heap ;)
+        *   Sacatelas
+        */ 
+
+        System.out.println("La ultima prueba");
     }
+
 }
+
