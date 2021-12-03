e31f95c147cb9998591d51b01ce6a1c25d1665be
Yaxca <yquerob.707@gmail.com>
2021-12-03 03:24:02 -0600

    Agregué métodos para inserción de nodos en AVL, también su javadoc

--- src/eda2_proyecto2_eq6_g5/ArbolAVL.java
+++ src/eda2_proyecto2_eq6_g5/ArbolAVL.java
@@ -14,10 +14,145 @@ public class ArbolAVL {
     
     public Nodo raiz;
     
+    public ArbolAVL(){
+        raiz=null;
+    }
+    public ArbolAVL(Nodo root){
+        raiz=root;
+    }
+    
+    /**
+     * Obtiene la altura del nodo
+     * Puede ser 1, 0 o -1
+     * @param elNodo objeto del tipo Nodo
+     * @return altura del nodo
+     */
     public int getAltura(Nodo elNodo){
         if( elNodo == null ){
             return 0;
         }
         return elNodo.altura;
     }
+    
+    public int getDiferencia(int alturaUno, int alturaDos){
+        int altura;    
+        //obtener altura máxima
+        
+        return 1; //devolver esa altura
+    }
+    
+    /**
+     * Obtiene la función equilibrio del Nodo
+     * @param padre objeto del tipo Nodo
+     * @return valor entero con la nueva altura para padre
+     */
+    public int equilibrio(Nodo padre){
+        int equilibrio;
+        if(padre==null)
+            return 0;
+        else
+            equilibrio = getAltura(padre.izq) - getAltura(padre.der); 
+        return equilibrio;
+    }
+ 
+    /**
+     * Insertar nodos en un árbol binario de búsqueda
+     * @param padre Nodo padre del árbol, regularmente la raíz
+     * @param hijo Nodo agregado por el usuario
+     * @param valorHijo valor del nuevo nodo
+     */
+    public void add(Nodo padre, Nodo hijo , int valorHijo){       
+        Nodo nodoNuevo;  
+        
+            //inserción normal
+        if(padre.valor>valorHijo){
+            if(padre.izq == null){
+                padre.izq = new Nodo(hijo.valor);
+                nodoNuevo= padre.izq;
+            }
+            else{
+                add(padre.izq,hijo,valorHijo);
+            }
+        }
+        if(padre.valor<valorHijo){
+            if(padre.der == null){
+                padre.der = new Nodo(hijo.valor);
+                nodoNuevo = padre.der;
+            } 
+            else{
+                add(padre.der,hijo,valorHijo);
+            } 
+        }
+        else{
+            System.out.println("No se permiten claves duplicadas");
+            return;
+        }
+            //actualizar altura del nodo Padre
+        padre.altura = 1 ;//+ equilibrio; 
+            
+            //equilibrio del padre
+        int equilibrio = equilibrio(padre);
+        //balancea(padre,
+            //balanceando árbol
+        if(equilibrio >1 && valorHijo < padre.izq.valor)
+            /*padre = rotaDerecha(padre);*/
+            
+        if(equilibrio < -1 && valorHijo > padre.der.valor)
+            /*padre = rotaIzquierda(padre)*/;
+        
+        if(equilibrio > 1 && valorHijo > padre.izq.valor)
+            /*padre.izq = rotaIzquierda(padre)
+            raiz = rotaDerecha(padre)*/
+            
+        if(equilibrio < -1 && valorHijo<padre.der.valor)
+            /*padre.der = rotaDerecha(padre);
+            raiz=rotaIzquierda(padre)*/valorHijo=1;
+         
+    }
+    
+    //funcion balancea
+    public void balancea(Nodo padre, int valorHijo){
+        int equilibrio = equilibrio(padre);
+        if(equilibrio >1 && valorHijo < padre.izq.valor)
+            /*raiz = rotaDerecha(padre);*/
+            
+        if(equilibrio < -1 && valorHijo > padre.der.valor)
+            /*raiz = rotaIzquierda(padre)*/;
+        
+        if(equilibrio < -1 && valorHijo<padre.der.valor)
+            /*padre.der = rotaIzquierda(padre);*/valorHijo=1;
+        
+        if(equilibrio > 1 && valorHijo > padre.izq.valor)
+            /*padre.izq = rotaDerecha(padre)*/
+            
+        if(equilibrio < -1 && valorHijo<padre.der.valor)
+            /*padre.der = rotaIzquierda(padre);*/valorHijo=1;
+    }
+    
+    //funcion rota izquierda
+    public Nodo rotaIzquierda(Nodo unNodo){
+        Nodo nuevaRaiz = unNodo.der;
+        Nodo izquierda = unNodo.izq;
+        
+        nuevaRaiz.izq = unNodo;
+        unNodo.der = izquierda;
+        
+        //actualiza alturas
+        
+        return nuevaRaiz;
+    }
+    
+    //funcion rota derecha
+    public Nodo rotaDerecha(Nodo unNodo){
+        Nodo nuevaRaiz = unNodo.izq;
+        Nodo izquierda = unNodo.der;
+        
+        nuevaRaiz.der = unNodo;
+        unNodo.izq = izquierda;
+        
+        //actualiza alturas
+        
+        return nuevaRaiz;
+    }
+  
 }
